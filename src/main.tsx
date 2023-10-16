34aed127ae7806d3666017fd7783b1b88edd9b82
hellpains <rustam2004sadulaev@mail.ru>
2023-10-16 21:24:51 +0300

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import App from '@/App'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
