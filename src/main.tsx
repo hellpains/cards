8dddc0b52ba020a007f2f5383833817a0bcf819c
hellpains <rustam2004sadulaev@mail.ru>
2023-10-16 21:31:45 +0300

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import App from '@/App'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
+import './styles/index.scss'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
